1d5570415fd3aa1b5a617e03da57b472317bff64
Anders Lundervold <anderslu@nih.no>
2024-11-07 17:00:03 +0100

    initial commit

--- web_app_2.py
+++ web_app_2.py
@@ -2,8 +2,8 @@ import streamlit as st
 import random
 
 st.title('Random Number Generator')
-st.subheader('Get a random number between 1 and 4')
+st.subheader('Get a random number between 1 and 4 for your session ID')
 
 if st.button('Generate'):
     number = random.randint(1, 4)
-    st.write(f'The generated number is: {number}')
+    st.write(f'The randomly generated session ID is: {number}')
